bebddef7b97e178a0f1f230a6664ec8b6ac3ddb4
AnatoliAlt <akademit24@gmail.com>
2024-11-14 18:19:43 +0100

    HW_18 add

---
+++ src/lesson_18__12_11_24_Modification_Static/homework18/NewCalculator.java
@@ -0,0 +1,28 @@
+package lesson_18__12_11_24_Modification_Static.homework18;
+
+public class NewCalculator {
+
+    public static int plus(int a, int b){
+        return a + b;
+    }
+    // Вычитение
+    public static int minus(int a, int b){
+        return a - b;
+    }
+    // Умножение
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    // Деление
+    public static int division(int a, int b){
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Сложение: " + plus(6,5));
+        System.out.println("Вычитание: " + minus(6,5));
+        System.out.println("Умножение: " + multiplication(5,5));
+        System.out.println("Деление: " + division(5,5));
+    }
+
+}
