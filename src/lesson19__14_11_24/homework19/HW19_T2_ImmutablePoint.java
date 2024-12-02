b55f86d388add557ed962d91d36f296f407a4ff3
AnatoliAlt <akademit24@gmail.com>
2024-12-02 19:52:54 +0100

    lesson19 add

--- src/lesson19__14_11_24/homework19/HW19_T2_ImmutablePoint.java
+++ src/lesson19__14_11_24/homework19/HW19_T2_ImmutablePoint.java
@@ -20,4 +20,45 @@ public class HW19_T2_ImmutablePoint {
         //Process finished with exit code 0
     }
 
+    /*                              Home Work 18
+                                           Task 1
+        Класс "Калькулятор"
+        Создайте класс Calculator, который будет предоставлять
+        базовые арифметические операции: сложение, вычитание, умножение и деление
+        Класс должен содержать:
+        Статические методы для каждой математической операции, принимающие два аргумента и возвращающие результат операции.
+         */
+    public static class NewCalculator {
+        // Сложение
+        public static double add(int a, int b) {
+            return a + b;
+        }
+
+        // Вычитение
+        public static double subtract(int a, int b) {
+            return a - b;
+        }
+
+        // Умножение
+        public static double multiply(int a, int b) {
+            return a * b;
+        }
+
+        // Деление
+        public static double divide(double a, double b) {
+            System.out.println("double division: ");
+            return a / b;
+        }
+
+        public static int divide(int a, int b) {
+            System.out.println("int division: ");
+            if (b == 0) {
+                // TODO хорошего решения нет.
+                return Integer.MAX_VALUE;
+            }
+            return a / b;
+        }
+
+
+    }
 }
